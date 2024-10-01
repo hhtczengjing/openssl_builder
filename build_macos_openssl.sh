a443994bc7817a22ac61abf6e91301a547cd3a2b
zengjing <hhtczengjing@gmail.com>
2024-10-01 14:19:19 +0800

    修改配置

---
+++ build_macos_openssl.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+sysOS=$(uname -s)
+NUM_THREADS=1
+if [ $sysOS == "Darwin" ]; then
+  #echo "I'm MacOS"
+  NUM_THREADS=$(sysctl -n hw.nCPU)
+elif [ $sysOS == "Linux" ]; then
+  #echo "I'm Linux"
+  NUM_THREADS=$(grep ^processor /proc/cpuinfo | wc -l)
+else
+  echo "Other OS: $sysOS"
+fi
+
+rm -rf build
+
+make clean
+./Configure darwin64-x86_64-cc --prefix="$(pwd)/build/openssl-x86_64" no-asm -mmacosx-version-min=10.15
+make -j4
+make install
+
+make clean
+./Configure darwin64-arm64-cc --prefix="$(pwd)/build/openssl-arm64" no-asm -mmacosx-version-min=10.15
+make -j4
+make install
+
+rm -rf $(pwd)/build/openssl
+mkdir -p $(pwd)/build/openssl/lib $(pwd)/build/openssl/bin
+
+lipo -create $(pwd)/build/openssl-arm64/lib/libssl.a $(pwd)/build/openssl-x86_64/lib/libssl.a -output $(pwd)/build/lib/libssl.a
+lipo -create $(pwd)/build/openssl-arm64/lib/libcrypto.a $(pwd)/build/openssl-x86_64/lib/libcrypto.a -output $(pwd)/build/lib/libcrypto.a
+
+lipo 
